2f60ba908fdf3a9caab0b859837b7b3aa11d8cb8
sdlima4025 <sergiolimafg3@gmail.com>
2025-08-13 19:40:39 -0300

    criado projeto validador de formulário.

---
+++ script.js
@@ -0,0 +1,70 @@
+let B7Validator = {
+  handleSubmit:(event) => {
+    event.preventDefault();
+    let send = true;
+
+    let inputs = form.querySelectorAll('input');
+
+    B7Validator.clearErrors();
+
+    for(let i=0;i<inputs.length;i++) {
+      let input = inputs[i];
+      let check = B7Validator.checkInput(input);
+      if(check !== true) {
+        send = false;
+        //exibir erro
+        B7Validator.showError(input, check)
+      }
+    }
+
+    if(send) {
+      form.submit();
+    }
+  },
+  checkInput:(input) => {
+    let rules = input.getAttribute('data-rules');
+
+
+    if(rules !== null) {
+      rules = rules.split('|');
+      for(let k in rules) {
+        let rDetails = rules[k].split('=');
+        switch(rDetails[0]) {
+          case 'required':
+            if(input.value =='') {
+              return 'Campo obrigatório, não pode ser vázio!';
+            }
+            break;
+            case 'min':
+
+
+            break;
+        }
+      }
+    }
+    return true;
+  },
+  showError:(input, error) => {
+    input.style.borderColor = '#FF0000';
+
+    let errorElement = document.createElement('div');
+    errorElement.classList.add('error');
+    errorElement.innerHTML = error;
+
+    // add menssagem após o input com ElementSimbling
+    input.parentElement.insertBefore(errorElement, input.ElementSimbling);
+  },
+  clearErrors:() => {
+    let inputs = form.querySelectorAll('input');
+    for(let i=0; i<inputs.length;i++) {
+      inputs[i].style='';
+    }
+
+    let errorElements = document.querySelectorAll('.error');
+    for(let i=0;i<errorElements.length;i++) {
+      errorElements[i].remove();
+    }
+  }
+};
+let form = document.querySelector('.b7validator');
+form.addEventListener('submit', B7Validator.handleSubmit);
