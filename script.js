bac4f7f88c2718a51ddcaa423474549c4a666ad0
sdlima4025 <sergiolimafg3@gmail.com>
2025-08-14 11:44:47 -0300

    criado regras de validação de inputs.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let B7Validator = {
-  handleSubmit:(event) => {
+  handleSubmit: (event) => {
     event.preventDefault();
     let send = true;
 
@@ -7,44 +7,53 @@ let B7Validator = {
 
     B7Validator.clearErrors();
 
-    for(let i=0;i<inputs.length;i++) {
+    for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
       let check = B7Validator.checkInput(input);
-      if(check !== true) {
+      if (check !== true) {
         send = false;
         //exibir erro
-        B7Validator.showError(input, check)
+        B7Validator.showError(input, check);
       }
     }
 
-    if(send) {
+    if (send) {
       form.submit();
     }
   },
-  checkInput:(input) => {
+  checkInput: (input) => {
     let rules = input.getAttribute('data-rules');
 
-
-    if(rules !== null) {
+    if (rules !== null) {
       rules = rules.split('|');
-      for(let k in rules) {
+      for (let k in rules) {
         let rDetails = rules[k].split('=');
-        switch(rDetails[0]) {
+        switch (rDetails[0]) {
           case 'required':
-            if(input.value =='') {
+            if (input.value == '') {
               return 'Campo obrigatório, não pode ser vázio!';
             }
             break;
-            case 'min':
-
-
+          case 'min':
+            if (input.value.length < rDetails[1]) {
+              return 'Campo tem que ter pelo menos' 
+               + rDetails + ' Caracters';
+            }
+            break;
+            case 'email':
+              if(input.value != '') {
+             let regex =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ 
+             if(!regex.test(input.value.toLocaleLowerCase())) {
+              return 'Digite um Email válido';
+             }
+            }
             break;
         }
       }
     }
     return true;
   },
-  showError:(input, error) => {
+  showError: (input, error) => {
     input.style.borderColor = '#FF0000';
 
     let errorElement = document.createElement('div');
@@ -54,17 +63,17 @@ let B7Validator = {
     // add menssagem após o input com ElementSimbling
     input.parentElement.insertBefore(errorElement, input.ElementSimbling);
   },
-  clearErrors:() => {
+  clearErrors: () => {
     let inputs = form.querySelectorAll('input');
-    for(let i=0; i<inputs.length;i++) {
-      inputs[i].style='';
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style = '';
     }
 
     let errorElements = document.querySelectorAll('.error');
-    for(let i=0;i<errorElements.length;i++) {
+    for (let i = 0; i < errorElements.length; i++) {
       errorElements[i].remove();
     }
-  }
+  },
 };
 let form = document.querySelector('.b7validator');
 form.addEventListener('submit', B7Validator.handleSubmit);
